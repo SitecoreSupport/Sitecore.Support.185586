f18d58b8d783baa3397e25dae9384db1d59e7b4c
DK\ELB <elb@sitecore.net>
2019-02-07 17:46:36 +0200

    no message

---
+++ src/Sitecore.Support.185586/Default.cs
@@ -0,0 +1,34 @@
+﻿using Sitecore.Configuration;
+using Sitecore.Globalization;
+using Sitecore.Security.Authentication;
+using System.Reflection;
+
+namespace Sitecore.Support.sitecore.login
+{
+    public class Default : Sitecore.sitecore.login.Default
+    {
+        protected override bool Login()
+        {
+            if (AuthenticationManager.Login(typeof(Sitecore.sitecore.login.Default).GetField("fullUserName", BindingFlags.NonPublic | BindingFlags.Instance).GetValue(this).ToString(), base.Password.Text, this.ShouldPersist()))
+            {
+                return true;
+            }
+            this.RenderError(Translate.Text("Your login attempt was not successful. Please try again."));
+            return false;
+
+        }
+
+        private bool ShouldPersist() =>
+    (!Settings.Login.DisableRememberMe && base.RememberMe.Checked);
+
+        private void RenderError(string text)
+        {
+            if (!string.IsNullOrEmpty(text))
+            {
+                base.FailureHolder.Visible = true;
+                base.FailureText.Text = text;
+            }
+        }
+       
+    }
+}
